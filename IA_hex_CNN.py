43050897060b8a5c46fec4de42791c925d0c213c
Lunatik4872 <gael.pailly@gmail.com>
2023-06-23 00:19:33 +0200

    CNN

--- IA_hex_CNN.py
+++ IA_hex_CNN.py
@@ -6,4 +6,13 @@ class NN_CNN :
 
         self.taille = taille
 
+        self.enter_NN = self.taille
+        self.hidden_NN = 20
+        self.pooling_NN = 5
+        self.hidden2_NN = 10
+        self.out_NN = self.taille
+
+        self.tmp = 0
+    
+    def forward(self,X) :
         
