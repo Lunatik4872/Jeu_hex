26a9b60fe4b07af137579abe5def8abf84b6b88b
Lunatik4872 <118389286+Lunatik4872@users.noreply.github.com>
2023-07-28 18:36:29 +0200

    Update IA_hex_CNN.py

--- IA_hex_CNN.py
+++ IA_hex_CNN.py
@@ -1,41 +1,99 @@
+import tensorflow as tf
 import numpy as np
+import os
 
 class NN_CNN :
 
     def __init__(self, taille) :
-
         self.taille = taille
 
         self.enter_NN = self.taille
         self.hidden_NN = 20
-        self.pooling_NN = 5
+        self.pooling_NN = tf.keras.layers.MaxPooling1D()
         self.hidden2_NN = 10
-        self.out_NN = self.taille
+        self.out_NN = self.taille**2
 
+        self.W = {}
         self.tmp = 0
+        self.load()
 
-        self.W = {'W1': np.random.randn(self.enter_NN,self.hidden_NN), 
-                  'W2': np.random.randn(self.hidden_NN,self.pooling_NN),
-                  'W3': np.random.randn(self.pooling_NN,self.hidden2_NN),
-                  'W4': np.random.randn(self.hidden2_NN,self.out_NN)} 
+        self.optimizer = tf.keras.optimizers.Adam()
     
-    def forward(self,X) :
-        X = np.array(X)
-        self.A = {}
+    def forward(self, X):
+        X = tf.convert_to_tensor(X, dtype=tf.float32)
+        self.a = {}
 
-    def softmax(self, Z):
-        e_x = np.exp(Z - np.max(Z, axis=0))
-        return e_x / np.sum(e_x, axis=0)
+        self.a['Z1'] = tf.matmul(X, self.W['W1'])
+        self.a['A1'] = tf.nn.relu(self.a['Z1'])
+        self.a['A1'] = tf.expand_dims(self.a['A1'], axis=0)
 
-    def softmax_derivative(self, Z):
-        softmax_output = self.softmax(Z)
-        return softmax_output * (1 - softmax_output)
+        self.a['pooling'] = self.pooling_NN(self.a['A1'])
+        self.a['A1'] = tf.squeeze(self.a['pooling'], axis=0)
+
+        self.a['Z2'] = tf.matmul(self.a['A1'], self.W['W2'])
+        self.a['A2'] = tf.nn.relu(self.a['Z2'])
+
+        self.a['Z3'] = tf.matmul(self.a['A2'], self.W['W3'])
+        self.output = tf.nn.relu(self.a['Z3'])
+
+
+        return self.output
     
-    def back_prop(self, X) :
-        ...
+    def backward(self, X, reward) :
+        X = tf.convert_to_tensor(X, dtype=tf.float32)
+        reward = tf.convert_to_tensor(reward, dtype=tf.float32)
+
+        with tf.GradientTape() as tape:
+            predictions = self.forward(X)
+            self.tmp += reward
+            loss = tf.reduce_mean(tf.square(reward - predictions))
+
+        grads = tape.gradient(loss, self.W.values())
+        self.optimizer.apply_gradients(zip(grads, self.W.values()))
     
-    def update(self) :
-        ...
+    def save(self):
+        poids_a_sauvegarder = {
+            'W1': self.W['W1'].numpy(),
+            'W2': self.W['W2'].numpy(),
+            'W3': self.W['W3'].numpy()
+        }
+        np.save("IA_W.npy", poids_a_sauvegarder)
+        np.save('tmp.npy', self.tmp)
     
-    def predict(X) :
-        ...
+    def load(self) :
+        if os.path.exists("AI_W.npy") and os.path.exists("tmp.npy"):
+            poids_charges = np.load("AI_W.npy", allow_pickle=True).item()
+            self.W['W1'].assign(poids_charges['W1'])
+            self.W['W2'].assign(poids_charges['W2'])
+            self.W['W3'].assign(poids_charges['W3'])
+            tmp = np.load("tmp.npy")
+            self.tmp = tmp
+        else :
+            self.tmp = 0
+
+            self.W = {'W1': tf.Variable(tf.random.normal((self.enter_NN, self.hidden_NN))),
+                    'W2': tf.Variable(tf.random.normal((self.hidden_NN, self.hidden2_NN))),
+                    'W3': tf.Variable(tf.random.normal((self.hidden2_NN, self.out_NN)))}
+            
+    """
+    def save(self):
+        model_dir = 'mon_modele/'
+        tf.saved_model.save(self, model_dir)
+        np.save(model_dir+'tmp.npy', self.tmp)
+    
+    def load(self):
+        loaded_model = tf.keras.models.load_model('mon_modele/')
+        tmp = np.load('mon_modele/tmp.npy')
+        self.tmp = tmp
+        
+    def load(self):
+        poids_charges = np.load("IA_W.npy", allow_pickle=True).item()
+        self.W['W1'].assign(poids_charges['W1'])
+        self.W['W2'].assign(poids_charges['W2'])
+        self.W['W3'].assign(poids_charges['W3'])
+        tmp = np.load('tmp.npy')
+        self.tmp = tmp
+        
+    def relu_derivative(self, Z):
+        return tf.where(Z > 0, tf.ones_like(Z), tf.zeros_like(Z))"""
+        
